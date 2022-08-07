a140187a137cfacdc2583fa46fc5939939452498
Manan5911 <85280203+Manan5911@users.noreply.github.com>
2022-08-07 17:29:28 +0530

    Add files via upload

---
+++ Project - INT/signup-js.js
@@ -0,0 +1,21 @@
+// alert("hello");
+
+function myFunction1() {
+    var x = document.getElementById("myInput1");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+function myFunction2() {
+    var x = document.getElementById("myInput2");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+  // document.getElementById("signupForm").onsubmit
